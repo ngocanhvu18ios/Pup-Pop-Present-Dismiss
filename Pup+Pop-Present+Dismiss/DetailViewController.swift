18ae4f9aa90639dc4bf59ebb85415f7937fce03a
Admin <ngocanhvu18ict@gmail.com>
2018-11-09 11:29:15 +0700

    init

---
+++ Pup+Pop-Present+Dismiss/DetailViewController.swift
@@ -0,0 +1,42 @@
+//
+//  DetailViewController.swift
+//  Pup+Pop-Present+Dismiss
+//
+//  Created by Admin on 11/9/18.
+//  Copyright © 2018 Admin. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func pop(_ sender: UIButton) {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func dismiss(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
